612d324c22f5d2daee65cd3a108707a595f70523
Andre Chaves <cchaves.andre@gmail.com>
2022-04-14 11:36:20 -0300

    Aula 20

---
+++ aula20a.py
@@ -0,0 +1,13 @@
+def lin():
+	print('----------------------------')
+
+
+# Programa principal  *** é obrigatório pular duas linhas vazias abaixo das funções
+lin()
+print('     SISTEMA DE ALUNOS      ')
+lin()
+print('  CADASTRO DE FUNCIONÁRIOS  ')
+lin()
+print('      ERRO DO SISTEMA       ')
+lin()
+
